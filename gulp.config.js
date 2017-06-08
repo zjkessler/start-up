b1ee1c23a937b99d06c82406f842a45d9bcea63d
Zach Kessler <zjkessler@gmail.com>
2017-06-08 13:05:20 -0600

    first commit

---
+++ gulp.config.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = function () {
+
+	var client = './src/client/';
+	var server = './src/server/';
+
+	var config = {
+
+		alljs: ['./src/**/*.js',
+				'./*js', '!' + client + 'bower_components/**/*.js'],
+
+		client: client,
+		index: client + 'index.html',
+		js: [
+			  client + '**/*.js',
+			'!' + client + '**/*.spec.js',
+		'!' + client + 'bower_components/**/*.js'],
+		jsIgnorePath: client,
+		css: [client + '**/*.css', '!' + client + 'bower_components/**/*.css'],
+		server: '',
+
+		//	Bower and Npm Locations
+		bower: {
+			json: require('./bower.json'),
+			directory: client + 'bower_components/',
+			ignorePath: '../..'
+		},
+		//NODE 
+		defaultPort: 8000,
+		nodeServer: server + 'server.js'
+	};
+
+	config.getWiredepDefaultOptions = function () {
+		var options = {
+			bowerJson: config.bower.json,
+			directory: config.bower.directory,
+			ignorePath: config.bower.ignorePath
+		};
+		return options;
+	};
+	return config;
+};
