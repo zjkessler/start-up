e6b2c409c4d447bea57c445b85fed2d276edc7c1
Zach Kessler <zjkessler@gmail.com>
2017-06-09 13:42:21 -0600

    added dependencies

---
+++ src/client/app.js
@@ -0,0 +1,7 @@
+var app = angular.module('StartUpApp', ['ui.router', 'ui.bootstrap', 'ngMaterial', 'ngMdIcons', 'ngAnimate'])
+
+app.config(['$stateProvider', '$locationProvider', '$mdThemingProvider', '$urlRouterProvider', function ($stateProvider, $locationProvider, $mdThemingProvider, $urlRouterProvider) {
+
+
+
+}]);
---
+++ src/server/server.js
@@ -0,0 +1,3 @@
+'use strict';
+var express = require('express');
+var app = express();
